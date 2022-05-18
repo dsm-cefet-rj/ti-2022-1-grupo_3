d1d73253d9631a5d5b7bd09b7973a140c910e1e7
Bruno Barbosa <91564840+BOliveiraBarbosa@users.noreply.github.com>
2022-05-18 14:36:06 -0300

    Rotas

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
 import Card from "../../components/Card"
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
 
 import "../../styles/Home.css"
 
 export default function Home() {
     return (
         <>
+        <Header/>
             <section>
                 <div id="feed-container">
                     <Card image="./exemplo1.jpeg" preco="R$ 30,00"/>
@@ -17,6 +20,7 @@ export default function Home() {
                     <Card image="./exemplo4.jpeg" preco="R$ 46,00"/>
                 </div>
             </section>
+        <Footer/>
         </>
     )
 }
--- frontend/src/pages/ProfileUser/index.jsx
+++ frontend/src/pages/ProfileUser/index.jsx
@@ -1,44 +1,59 @@
+import Header from "../../components/Header"
+import Card from "../../components/Card"
+import Footer from "../../components/Footer"
+
 import "../../styles/ProfileUser.css"
 
 export default function ProfileUser() {
     return(
         <main>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
-            <div class="profile">
-                <img class="user-pp-wrapper" src="public/images/placeholder_usuario.jpeg" alt="Foto de Perfil"/>
-                <h2>Nome usuário <span class="badge badge-secondary"></span></h2>
-                <a href=''>Editar Perfil</a>
-            </div>
-
-            <h3>Últimos Cortes <span class="badge badge-secondary"></span></h3>
-
-            <button id="button_corte" type="button" class="btn btn-primary">Adicionar novo corte</button>
-
-            <div class="card" style="width: 18rem; display: inline-block; margin-bottom: 20px;">
-                <img class="card-img-top" src="public/images/hair_2.jpg" alt="Primeiro Corte" />
-                <div class="card-body">
-                    <p class="card-text">Descrição sobre o corte 1</p>
+        <Header/>
+        <header class="user-profile-container">
+            <div class="user-profile">
+                <div class="user-pp-wrapper">
+                    <img
+                        src="./exemplo1.jpeg"
+                        alt="Foto de perfil"
+                    />
                 </div>
+
+                <h2 class="user-name">Nome</h2>
+
+                <p class="user-description">
+                    Breve descrição do Usuario Breve descrição do Usuario
+                </p>
+
+                <button class="edit profile" type="button">
+                    Editar perfil
+                </button>
+                
+            </div>
+        </header>
+        <section>
+            <div>
+                <ul class="profile-tabs">
+                    <a class="selected" href="#">
+                        <li>Meus cortes</li>
+                    </a>
+                    <a href="especialidade_cabelereiro.html">
+                        <li>Minhas especialidades</li>
+                    </a>
+                </ul>
+            </div>
+            <div id="feed-container">
+                <Card image="./exemplo1.jpeg" preco="R$ 30,00"/>
+                <Card image="./exemplo1.jpeg" preco="R$ 30,00"/>
             </div>
 
-            <div class="card" style="width: 18rem; display: inline-block; margin-left: 20px;">
-                <img class="card-img-top" src="public/images/hair_3.jpg" alt="Segundo Corte" />
-                <div class="card-body">
-                    <p class="card-text">Descrição sobre o corte 2</p>
-                </div>
-            </div>
-
+            <br/>
             <nav>
                 <ul>
                     <a href="#"><li>Suas Avaliações</li></a>
                     <a href="#"><li>Ultimos Locais</li></a>
                 </ul>
             </nav>
-
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-        </main>
+        </section>
+        <Footer/>
+    </main>
     )
 }
