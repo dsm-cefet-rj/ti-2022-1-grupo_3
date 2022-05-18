62feb82580f0659affa265421a02e45d63fd29a6
AndreRX <54478884+AndreRX@users.noreply.github.com>
2022-05-18 12:09:47 -0300

    Mudanças no CSS dos Cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Home from "./pages/Home";
 import Footer from "./components/Footer"
 import Header from "./components/Header";
+import CreatePublication from "./pages/CreatePublication"
+import ProfileUser from "./pages/ProfileUser"
+import ProfileCabelereiro from "./pages/ProfileCabelereiro"
 
 function App() {
   return (
@@ -9,7 +13,7 @@ function App() {
       <Header/>
       <Home/>
       <Footer/>
-    </div>
+   </div>
   );
 }
 
