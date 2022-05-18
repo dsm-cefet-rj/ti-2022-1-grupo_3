50a936ca053365526d4e44fa88382a319cef5478
GE28 <44209041+GE28@users.noreply.github.com>
2022-05-17 22:25:09 -0300

    Ajusta labels do formulário de registro

--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -5,22 +5,22 @@ export default function Register() {
             <legend>Cadastre-se</legend>
 
             <label for="name" class="form-content">Nome:</label>
-            <input type="text" name="Nome" class="form-content"/>
+            <input type="text" id="name" name="name" class="form-content"/>
 
             <label for="username" class="form-content">Username:</label>
-            <input type="text" name="username" class="form-content"/>
+            <input type="text" id="username" name="username" class="form-content"/>
 
-            <label class="form-content">Data de Nascimento:</label>
-            <input type="date" class="form-content"/>
+            <label for="birthday" class="form-content">Data de Nascimento:</label>
+            <input type="date" id="birthday" name="birthday" class="form-content"/>
 
-            <label class="form-content">E-mail:</label>
-            <input type="email" class="form-content"/>
+            <label for="email" class="form-content">E-mail:</label>
+            <input type="email" id="email" name="email" class="form-content"/>
 
-            <label class="form-content">Senha:</label>
-            <input type="password" class="form-content"/>
+            <label for="password" class="form-content">Senha:</label>
+            <input type="password" id="password" name="password" class="form-content"/>
 
-            <label class="form-content">Confirmação Senha:</label>
-            <input type="password" class="form-content"/>
+            <label for="confirm-password" class="form-content">Confirmação Senha:</label>
+            <input type="password" id="confirm-password" name="confirm-password" class="form-content"/>
 
             <button type="submit" id="submit">Cadastre-se</button>
         </form>
