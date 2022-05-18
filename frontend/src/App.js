74dbe9b26d62a32d87d83ca2934be2cdf59e877d
Bruno Barbosa <91564840+BOliveiraBarbosa@users.noreply.github.com>
2022-05-18 14:04:30 -0300

    Teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import CreatePublication from "./pages/CreatePublication";
+import EspecialidadeCabelereiro from "./pages/EspecialidadeCabelereiro";
 import Home from "./pages/Home";
-import Footer from "./components/Footer"
-import Header from "./components/Header"
+import ProfileCabelereiro from "./pages/ProfileCabelereiro";
+import ProfileUser from "./pages/ProfileUser";
+import Register from "./pages/Register";
+import Schedule from "./pages/Schedule";
 
 function App() {
   return (
-    <div className = "App">
-      <Header/>
-      <Home/>
-      <Footer/>
-   </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/me" element={<ProfileUser/>}/>
+        <Route path="/me/newPublicarion" element={<CreatePublication/>}/>
+        <Route path="/me/schedule" element={<Schedule/>}/>
+        <Route path="/cabeleireiro" element={<ProfileCabelereiro/>}/>
+        <Route path="/cabeleireiro/especialidade" element={<EspecialidadeCabelereiro/>}/>
+      </Routes>
+    </Router>
   );
 }
 
