c107741534f1c94bc597df6e26bdf380b9bc15f5
GE28 <44209041+GE28@users.noreply.github.com>
2022-05-25 11:30:32 -0300

    Renomeia arquivos

---
+++ frontend/src/pages/Schedule/index.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+import CardSchendule from "../../components/CardSchedule";
+
+export default function Schedule(props) {
+    return(
+        <>
+        <Header/>
+            <div className="main-container" id="form-container">
+                <legend>Sua Agenda!</legend>
+                <br></br>
+            </div>
+
+            <div id="feed-container">
+                {
+                    props.bookings.map((booking)=>{
+                        return(
+                            <>
+                                <CardSchendule key={booking.id} booking ={booking}/>
+                            </>
+                        )
+                    })
+                }
+            </div>
+
+        <Footer/>
+        </>
+    );
+}
