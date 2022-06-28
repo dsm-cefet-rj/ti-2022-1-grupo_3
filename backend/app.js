0e36fe6e413f7db72af958cce2d0a5e75ec5e558
Bruno Barbosa <91564840+BOliveiraBarbosa@users.noreply.github.com>
2022-06-28 19:27:39 -0300

    Installation

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,27 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var homeRouter = require('./routes/home');
-var productsRouter = require('./routes/products');
-var bookingsRouter = require('./routes/bookings');
-var sellersRouter = require('./routes/sellers');
+var indexRouter = require("./routes/index");
+var productsRouter = require("./routes/products");
+var bookingsRouter = require("./routes/bookings");
+var sellersRouter = require("./routes/sellers");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', homeRouter);
-app.use('/products', productsRouter);
-app.use('/bookings', bookingsRouter);
-app.use('/sellers', sellersRouter);
+app.use("/", indexRouter);
+app.use("/products", productsRouter);
+app.use("/bookings", bookingsRouter);
+app.use("/sellers", sellersRouter);
 
 
 module.exports = app;
