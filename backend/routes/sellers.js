b660e974ae5f767d9875a7fdd353c14069d14474
GE28 <44209041+GE28@users.noreply.github.com>
2022-06-29 15:32:29 -0300

    Atualiza rota de cabeleireiros

--- backend/routes/sellers.js
+++ backend/routes/sellers.js
@@ -1,69 +1,64 @@
 var express = require('express');
 var router = express.Router();
+var sellerModel = require('../models/sellers');
 const bodyParser = require('body-parser');
 
 router.use(bodyParser.json())
 
-var sellers = [
-  {
-    "id": 1,
-    "name": "Cabeleireiro A",
-    "username": "CabeA",
-    "birthday": "06/11/2000",
-    "email": "cabeleireiroa@gmail.com",
-    "password": ""
-  },
-  {
-    "id": 2,
-    "name": "Cabeleireiro B",
-    "username": "CabeB",
-    "birthday": "26/04/2001",
-    "email": "cabeleireirob@gmail.com",
-    "password": ""
-  }
-];
-
 /* GET (read) sellers listing. */
-router.route("/").get((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json(sellers);
+router.route("/").get(async (req, res, next) => {
+  try {
+    const seller = await sellerModel.find();
+    res.status(200).json(seller || {});
+  } catch (err) {
+    res.status(404).json({});
+  }
 });
 
-router.route("/:id").get((req, res, next) => {
-  const id = parseInt(req.params.id);
-  const seller = sellers.filter((s) => (s.id = id));
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json(seller);
+router.route("/:id").get(async (req, res, next) => {
+  try {
+    const seller = await sellerModel.findById(req.params.id);
+    res.status(200).json(seller || {});
+  } catch (err) {
+    res.status(404).json({});
+  }
 });
 
 /* POST (create) seller. */
-router.route("/").post((req, res, next) => {
-  let nextId = 1 + sellers.map((s) => s.id).reduce((x, y) => Math.max(x, y));
-  let seller = { ...req.body, id: nextId };
-  sellers.push(seller);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json(seller);
+router.route("/").post(async (req, res, next) => {
+  let newSeller = new sellerModel({...req.body});
+
+  try {
+    const response = await newSeller.save();
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({});
+  }
 });
 
 /* PUT (update) seller. */
-router.route("/:id").put((req, res, next) => {
-  let index = sellers.map((s) => s.id).indexOf(req.params.id);
-  sellers.splice(index, 1, { id: index, ...req.body });
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json(req.body);
+router.route("/:id").put(async (req, res, next) => {
+	try {
+		const response = await sellerModel
+			.findByIdAndUpdate(req.params.id, {
+				...req.body
+			});
+
+		res.status(200).json(response);
+	} catch (err) {
+		res.status(500).json({});
+	}
 });
 
 /* DELETE (delete) seller. */
-router.route("/:id").delete((req, res, next) => {
-    const id = parseInt(req.params.id);
-    sellers = sellers.filter((s) => s.id != id);
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.json(req.params.id);
+router.route("/:id").delete(async (req, res, next) => {
+  try {
+    const response = await sellerModel.findByIdAndRemove(req.params.id);
+    res.status(200).json(req.params.id);
+  } catch (err) {
+    res.status(404).json({});
+  }
 });
 
 module.exports = router;
