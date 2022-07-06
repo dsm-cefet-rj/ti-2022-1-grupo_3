f3062daf60a4b7553688c1e3b25f0c2af293cd3b
AndreRX <54478884+AndreRX@users.noreply.github.com>
2022-07-06 00:35:27 -0300

    iniciando autenticacao

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var cors = require("cors");
 
 var indexRouter = require("./routes/index");
@@ -25,7 +27,58 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser('12345-67890-73567-54123'));
+
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-73567-54123',
+    saveUninitialized: false, 
+    resave: false,
+    store: new FileStore()
+}));
+
+function auth(req, res, next) {
+    console.log(req.headers);
+
+    if(!req.session.user) {
+        var authHeader = req.headers.authorization;
+        if(!authHeader) {
+            var err = new Error('You are not authenticates!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            next(err);
+            return;
+        }
+
+        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+
+        if (user == 'admin' && pass=='password') {
+            req.session.user = 'admin';
+            next();
+        }else {
+            var err = new Error('You are not authenticates!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            next(err);
+        }
+
+    } else {
+        if (req.session.user == 'admin') {
+            console.log('req.session: ', req.session);
+            next();
+        }
+        else {
+            var err = new Error('You are not authenticates!');
+            err.status = 401;
+            next(err);
+        }
+    }  
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
