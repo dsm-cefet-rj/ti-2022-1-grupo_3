0f9b0d7e02261d244d0ac7884b13f7b4a798fe74
GE28 <44209041+GE28@users.noreply.github.com>
2022-07-12 20:02:57 -0300

    Interrompe o uso de cookies

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
 require("dotenv").config();    
+var cookieParser = require("cookie-parser");
+var cors = require("cors");
 var express = require("express");
-var session = require("express-session");
+var logger = require("morgan");
 var mongoose = require('mongoose');
 var passport = require('passport');
 var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
+var session = require("express-session");
 
+var bookingsRouter = require("./routes/bookings");
 var indexRouter = require("./routes/index");
 var productsRouter = require("./routes/products");
-var bookingsRouter = require("./routes/bookings");
 var sellersRouter = require("./routes/sellers");
 var usersRouter = require("./routes/users");
 
@@ -25,15 +25,8 @@ mongoose
 .catch((err) => console.log(err));
   
 app.use(logger("dev"));
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(session({ 
-    secret: process.env.SECRET_KEY, 
-    saveUninitialized:true,
-    cookie: { maxAge: Number.parseInt(process.env.COOKIE_MAX_AGE) },
-    resave: false 
-}));
 app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
