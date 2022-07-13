430f8b85d01016ee62cd463e1fa293c0b72ea233
GE28 <44209041+GE28@users.noreply.github.com>
2022-07-12 21:00:26 -0300

    Reverte para versão funcional do passport

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,9 @@
 var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
-var JwtStrategy = require("passport-jwt").Strategy;
 var ExtractJwt = require("passport-jwt").ExtractJwt;
+var JwtStrategy = require("passport-jwt").Strategy;
+var LocalStrategy = require("passport-local").Strategy;
 var User = require("../models/users");
-var {sign} = require("jsonwebtoken");
+var { sign } = require("jsonwebtoken");
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -14,20 +14,15 @@ options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = process.env.SECRET_KEY;
 
 exports.getToken = function (user) {
-  return sign(user, process.env.SECRET_KEY, { expiresIn: "1 h" });
+  return sign(user, process.env.SECRET_KEY, { expiresIn: "1d" });
 };
 
 exports.jwtPassport = passport.use(
   new JwtStrategy(options, function (jwt_payload, done) {
     User.findOne({ _id: jwt_payload._id }, (err, user) => {
-      if (err) {
-        return done(err, false);
-      }
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, true);
-      }
+      if (err) return done(err, false);
+      if (user) return done(null, user);
+      return done(null, true);
     });
   })
 );
